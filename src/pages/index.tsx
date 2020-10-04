6e50b92c4a42b620df96b9d838a716d8c38a0822
Maick Souza <maick_a_s@msn.com>
2020-10-04 11:49:37 -0300

    chore(web): Using Prismic SDK

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,17 @@
 import SEO from '@/components/SEO';
 import { GetServerSideProps } from 'next';
+import Link from 'next/link';
 import { Title } from '../styles/pages/Home';
-
-interface IProduct {
-  id: string;
-  title: string;
-}
+import { client } from '@/lib/prismic';
+import Prismic from 'prismic-javascript';
+import PrismicDOM from 'prismic-dom';
+import { Document } from 'prismic-javascript/types/documents';
 
 interface HomeProps {
-  recommendedProducts: IProduct[];
+  recommendedProducts: Document[];
 }
 
 export default function Home({ recommendedProducts }: HomeProps) {
-  async function handleSum() {
-    console.log(process.env.NEXT_PUBLIC_API_URL)
-
-    const math = (await import('@/lib/math')).default;
-
-    alert(math.sum(3, 5));
-  }
 
   return (
     <div>
@@ -35,25 +28,31 @@ export default function Home({ recommendedProducts }: HomeProps) {
           {recommendedProducts.map(recommendedProduct => {
             return (
               <li key={recommendedProduct.id}>
-                {recommendedProduct.title}
+                <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                  <a>
+                    {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                  </a>                   
+                </Link>                
               </li>
             );
           })}
         </ul>
       </section>
-
-      <button onClick={handleSum}>Sum!</button> 
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`)
-  const recommendedProducts = await response.json();
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at('document.type', 'product')
+  ]);
+
+  console.log(recommendedProducts.results[0].data.title);
+
 
   return {
     props: {
-      recommendedProducts
+      recommendedProducts: recommendedProducts.results,
     }
   }
 }
